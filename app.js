ff15fa33f429858cff2a5fc297ef635748a9eb2f
Sandra-Kao <63223781+Sandra-Kao@users.noreply.github.com>
2021-03-08 14:03:13 +0800

    Simple Vue.js work
    See codepen demo: https://codepen.io/K-SY/pen/mdOzJeB

---
+++ app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data() {
+        return {
+            name: 'Snadra',
+            age: 20,
+            imgSrc: 'https://vuejs.org/images/logo.png'
+        }
+    },
+    method: {}
+}).mount('#assignment')
